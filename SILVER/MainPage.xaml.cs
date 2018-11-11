c144f490f9a91c3b3c85bf2a4ea567a8ad6a6f8d
liyan1234567 <18729347300@163.com>
2018-11-11 12:00:09 +0800

    second

--- SILVER/MainPage.xaml.cs
+++ SILVER/MainPage.xaml.cs
@@ -18,5 +18,10 @@ namespace SILVER
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
     }
 }
